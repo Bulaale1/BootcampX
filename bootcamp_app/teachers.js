63109f74ebae3f44e29a61ca97e5d3881ec0d228
Yasin Hussein <bulaale116@gmail.com>
2024-02-09 09:29:29 -0700

    Parameterize teachers and students queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,17 +6,22 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-// const queryParameters = process.argv[2];
+// const cohortName = process.argv[2];
+const limit = process.argv[2] || 'JUL02';
+// Store all potentially malicious values in an array.
+const values = [limit];
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name like $1
 ORDER BY teacher;
-`)
+  `;
+
+pool.query(queryString,values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${process.argv[2]} : ${user.teacher}`);
